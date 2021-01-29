b7896ef5a40cfaf40ed9afaec7e6419ad45ba324
BonDu2021 <nguyenduydiepks@gmail.com>
2021-01-29 12:07:32 +0700

    Kiem tra so chan

--- my.js
+++ my.js
@@ -1,3 +1,10 @@
-function showmassage() {
-    alert("xin chao cac ban");
+function Kiemtraso(){
+    let number = document.getElementById("number").value;
+    if(number % 2==0){
+        document.getElementById("result").innerHTML= "Số này là số chẵn";
+    } else{
+        document.getElementById("result").innerHTML= "Số này không phải là số chẵn"; 
+    }
+    
+    
 }
